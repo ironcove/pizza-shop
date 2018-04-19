2dc959c9eb517882f1f354312c2b2a0cfcc33232
Patrick Monahan <patrickmonahan@Patricks-iMac.local>
2018-04-18 19:59:14 -0700

    create topnavbar file

--- src/App.js
+++ src/App.js
@@ -7,15 +7,21 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <img src={logo} className="App-logo" alt="logo" title="Iron Cove Solutions"/>
+          <h1 className="App-title">Iron Cove Solutions</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <p className="App-text">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <h2 className="h2">Single Sign On <a href="../technology/okta">SSO</a></h2>
       </div>
     );
   }
 }
+
+
 
 export default App;
